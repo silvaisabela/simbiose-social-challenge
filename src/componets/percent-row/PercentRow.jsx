04c48122e8ba66de21206e39bd23142d9e13acb1
silvaisabela <isabelai27@hotmail.com>
2019-07-28 14:29:57 -0300

    Atualizar PercentRow para utilizar função

--- src/componets/percent-row/PercentRow.jsx
+++ src/componets/percent-row/PercentRow.jsx
@@ -3,15 +3,10 @@ import './PercentRow.css'
 
 class PercentRow extends Component{
 
-    constructor(props){
-        super(props)
-
-    }
-
     render(){
         return(
             <div className="percent-container">
-                <p className="percent-row" style={{width: `${this.props.percent}`}}>{this.props.percent}</p>
+                <p className="percent-row" style={{width: `${this.props.percent}%`}}>{this.props.percent}%</p>
             </div>
         )
     }

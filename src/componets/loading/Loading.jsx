ebd75f8264385478b80c671ab44afb563c8efe56
silvaisabela <isabelai27@hotmail.com>
2019-07-28 23:53:54 -0300

    Criar componente Loading

---
+++ src/componets/loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loading.css'
+
+function loading() {
+    return (
+        <div className="loader">
+            <div></div>
+        </div>
+    )
+}
+
+export default loading
